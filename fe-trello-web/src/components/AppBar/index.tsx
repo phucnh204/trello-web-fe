e43469f053a51cdb04cfb8b22520e8b1068abf66
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-01-27 22:18:09 +0700

    Done Menus 50%

--- fe-trello-web/src/components/AppBar/index.tsx
+++ fe-trello-web/src/components/AppBar/index.tsx
@@ -1,23 +1,38 @@
-import { Box } from '@mui/material';
-import React from 'react'
-import ModeSelect from '../ModeSelect';
-
+import { Box } from "@mui/material";
+import ModeSelect from "../ModeSelect";
+// import AppsIcon from "@mui/icons-material/Apps";
+import "./style.css";
+import Workspace from "./Menus/Workspace";
+import Recent from "./Menus/Recent";
 function AppBar() {
   return (
-    <div>
-      <Box
-        sx={{
-          backgroundColor: "primary.light",
-          width: "100%",
-          height: "60px",
-          display: "flex",
-          alignItems: "center",
-          // justifyContent: "end",
-        }}
-      >
+    <Box
+      sx={{
+        backgroundColor: "",
+        width: "100%",
+        height: "60px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: "2",
+      }}
+    >
+      <Box sx={{ gap: "0.5", display:"flex" , alignItems:"center"}}>
+        {/* <AppsIcon sx={{ color: "" }} /> */}
+        <img
+          className="trello-logo"
+          src="https://logos-world.net/wp-content/uploads/2021/02/Trello-Logo.png"
+          alt=""
+          style={{ width: "100px", height: "50px", marginLeft: "20px" }}
+        />
+        <Workspace />
+        <Recent/>
+      </Box>
+
+      <Box sx={{}}>
         <ModeSelect />
       </Box>
-    </div>
+    </Box>
   );
 }
 
