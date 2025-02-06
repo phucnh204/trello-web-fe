42993a729e1c770ec560bfbc41e85d97b79e6e96
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-06 11:43:05 +0700

    Update index.tsx

--- fe-trello-web/src/pages/Boards/BoardContent/index.tsx
+++ fe-trello-web/src/pages/Boards/BoardContent/index.tsx
@@ -15,7 +15,7 @@ import {
   Tooltip,
   Typography,
 } from "@mui/material";
-
+// 
 import React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
