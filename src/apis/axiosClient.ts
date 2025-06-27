b9b211687e0a23749669b4a7a23134a67b29f8b4
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-06-27 17:26:31 +0700

    config file env.

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// const axiosClient = axios.create({
+//   baseURL: "http://localhost:8000",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// export default axiosClient;
+// src/api/axiosClient.ts hoặc tương tự
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.VITE_API_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
