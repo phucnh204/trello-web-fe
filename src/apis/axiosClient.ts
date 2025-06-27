9efe662d6d36781d2b71e2a9cc78cda3096547e7
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-06-27 17:58:28 +0700

    Update axiosClient.ts

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -12,7 +12,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.VITE_API_URL || "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
